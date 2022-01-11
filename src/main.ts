aca1a18d7223b7e115b164ef68b40fd5d897f936
Edwin Fajardo <alexander_suarez1997@hotmail.com>
2022-01-11 15:22:12 -0500

    feat: request timeout is added

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { TimeOutInterceptor } from './shared/infraestructure/interceptors/timeout.interceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	const logger = new Logger();
+
+	app.useGlobalInterceptors(
+		new TimeOutInterceptor()
+	);
 
 	app.useGlobalPipes(
 		new ValidationPipe({
@@ -21,10 +26,15 @@ async function bootstrap() {
 		.setTitle('To-do')
 		.setDescription('To-do')
 		.setVersion('1.0')
+		.addBearerAuth()
 		.build();
 
 	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('docs', app, document);
+	SwaggerModule.setup('docs', app, document, {
+		swaggerOptions: {
+			filter: true
+		}
+	});
 
 	await app.listen(process.env.PORT || 3000);
 
